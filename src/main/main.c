d55f2607c0194e924b62a31ce43d9f6698e73700
Rodrigo Lins <rlinsdev@gmail.com>
2023-01-23 16:09:18 -0300

    Verify args

--- src/main/main.c
+++ src/main/main.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 08:04:02 by rlins             #+#    #+#             */
-/*   Updated: 2023/01/23 14:35:27 by rlins            ###   ########.fr       */
+/*   Updated: 2023/01/23 16:08:36 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,26 @@
  */
 int	main(int argc, char **argv, char **envp)
 {
+	if (argc != 5 && argc != 6)
+	{
+		printf("Error in arguments AAAAA\n");
+		exit(1);
+	}
+	else
+	{
+		bool error = false;
+		int nb_philo = ft_ato_long(argv[1], &error);
+		int time_die = ft_ato_long(argv[2], &error);
+		int time_eat = ft_ato_long(argv[3], &error);
+		int time_sleep = ft_ato_long(argv[4], &error);
+		if (argc == 6) {
+			int times_exit = ft_ato_long(argv[5], &error);
+		}
+	}
+
 	// int result = race_cond();
 	// int result = deadlock();
 	// int result = func_semaphore();
-	circle();
+	//circle();
 	return (0);
 }
